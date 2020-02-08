bc07c9c93710028362f1619d32798046659a9eaf
Kyle Tan <kylebrandontan@gmail.com>
2020-02-08 15:47:17 +0800

    Add Order Model with spec

---
+++ app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ApplicationRecord
+  belongs_to :warehouse
+  validates :customer_name, presence: true
+  validates :uuid, presence: true
+end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,6 +1,7 @@
 class Warehouse < ApplicationRecord
   has_many :stocks
   has_many :products, through: :stocks
+  has_many :orders
 
   validates :street, presence: true
   validates :city, presence: true
