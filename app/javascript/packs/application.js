81f1ea52a652d984a54a2a12c525118289a1adf7
Marc Dagatan <marc.dagatan@gmail.com>
2020-02-22 11:45:26 +0800

    Add bootstrap classes to style components, add navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,16 @@
-import Rails from '@rails/ujs';
-import Turbolinks from 'turbolinks';
-import * as ActiveStorage from '@rails/activestorage';
-import 'bootstrap';
-import popper from 'popper.js';
-import jquery from 'jquery';
-import '../application.scss';
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import popper from "popper.js";
+import jquery from "jquery";
+import "../application.scss";
 
 window.Popper = popper;
 window.jQuery = jquery;
 window.$ = jquery;
 
+import "bootstrap";
+
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
-ReactRailsUJS.detectEvents();
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
   resources :products, only: %i[index show edit new create update destroy]
   resources :warehouses, only: %i[index show edit new create update destroy]
   resources :stocks, only: %i[index new show create]
