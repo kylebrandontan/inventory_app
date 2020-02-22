d856713cfecb457a7d99f4ac67f256b7b2fa1125
Marc Dagatan <marc.dagatan@gmail.com>
2020-02-22 10:54:54 +0800

    Add orders and order items crud

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :products, only: %i[index show edit new create update destroy]
   resources :warehouses, only: %i[index show edit new create update destroy]
   resources :stocks, only: %i[index new show create]
+  resources :orders, only: %i[index new show create update destroy edit] do
+    resources :order_items, only: %i[create destroy]
+  end
 end
