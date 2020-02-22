7de18f589dc949524d198d6016d5a4f27cf85d10
Kyle Tan <kylebrandontan@gmail.com>
2020-02-22 18:04:45 +0800

    Add increment for OrderItem

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,11 +2,18 @@ class OrderItemsController < AdminController
   before_action :set_order
 
   def create
-    @order_item = @order.order_items.build(order_items_params)
+    @order_item = @order.order_items.find_or_initialize_by(product_id: order_items_params[:product_id].to_i)
+    if @order_item.new_record?
+      flash.notice = "Successfully added order item!"
 
+      @order_item.quantity = order_items_params[:quantity].to_i
+    else
+      flash.notice = "Successfully added order item!"
+      @order_item.quantity += order_items_params[:quantity].to_i
+    end
     @order_item.save!
-
     redirect_to order_path(@order)
+    end
   end
 
   def destroy
@@ -27,4 +34,3 @@ class OrderItemsController < AdminController
   def order_items_params
     params.require(:order_item).permit(:quantity, :product_id)
   end
-end
